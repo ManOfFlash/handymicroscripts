b3d60ee3dc738e18f744211b2f732741a890b20c
ManOfFlash <3252003+ManOfFlash@users.noreply.github.com>
2021-05-16 14:19:46 +0700

    Refacror

--- _install.sh
+++ _install.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-#untested!
+
 dest_dir=/usr/bin
-files=clone exec_SQL_script.sh ffd fp fsd _install.sh koprobe lans nag newsh stopwatch.sh vlans
-for fl in files
+files="clone exec_SQL_script.sh ffd fp fsd koprobe lans nag newsh stopwatch.sh vlans"
+
+for fl in $files
 do
     cp $fl $dest_dir
 done
+
 echo Copying done. Press enter to continue.
 read
